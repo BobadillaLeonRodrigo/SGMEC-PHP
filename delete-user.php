0138bdd7f4b93ff1e8ce4eeb08459d929198f983
BobadillaLeonRodrigo <al222110815@gmail.com>
2023-06-21 21:52:06 -0600

    Limpieza de codigo.

--- delete-user.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("Connection.php");
-    $con=conectar();
-        $id_U=$_GET['id'];
-            $sql="DELETE FROM Usuarios WHERE id_usuario='$id_U'";
-                $query=mysqli_query($con,$sql);
-                    if($query){
-                        Header("Location: dashboard.php");
-                    }
